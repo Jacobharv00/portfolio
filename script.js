306b840a7a5e8546841d94413e819ad44b97ea9a
jacob <jacobharv00@gmail.com>
2021-05-20 12:58:06 -0600

    updated site

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const button = document.createElement('button')
+button.innerHTML = "Click Me"
+document.body.appendChild(button)
+
+button.addEventListener('click', (e) => {
+    alert('You clicked a button')
+})
+
+
+
+
+
 
 
 
