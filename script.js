097d1f915588e7bd4a15df2c725e518ee09226f0
jacob <jacobharv00@gmail.com>
2021-05-22 15:20:46 -0600

    progress on dark/light mode

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 // Dark || Light Mode
 const btn = document.querySelector('.btn-toggle')
+const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)')
 
 const currentTheme = localStorage.getItem('theme')
-if (currentTheme === 'dark') {
-    document.body.classList.add('dark-theme')
+    if (currentTheme === 'dark') {
+    document.body.classList.toggle('dark-theme')
+    } else if (currentTheme === 'light') {
+    document.body.classList.toggle('light-theme')
 }
 
 btn.addEventListener('click', function () {
-    document.body.classList.toggle('dark-theme')
-   
-    let theme = 'light'
-    if (document.body.classList.contains('dark-theme')) {
-    theme = 'dark'
-    }
-    localStorage.setItem('theme', theme)
+    if (prefersDarkScheme.matches) {
+        document.body.classList.toggle('light-theme')
+        let theme = document.body.classList.contains('light-theme')
+        ? 'light'
+        : 'dark'
+    } else {
+        document.body.classList.toggle('dark-theme')
+        let theme = document.body.classList.contains('dark-theme')
+        ? 'dark'
+        : 'light'
+}
+        localStorage.setItem('theme', theme)
 })
-// Button not working yet!!!!!
+
+
+
+
 
 
 
